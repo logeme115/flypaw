ab6c52baac9eee42067d02010cd825b5bebdd687
Andrew <andgro115@gmail.com>
2022-10-02 11:22:16 -0400

    DBUG

--- drone/flypawPilot/flypawPilot.py
+++ drone/flypawPilot/flypawPilot.py
@@ -534,17 +534,30 @@ class FlyPawPilot(StateMachine):
         return "waypoint_entry"
 
     @timed_state(name="iperf",duration = 20)
-    async def iperf(self, drone: Drone):#IPERF STATE TO BS ONLY
+    async def iperf(self, drone: Drone):
         print("starting iperf state")
         logState(self.logfiles['state'], "iperf")
-
+        iperfObjArr = []
+        for resource in self.resources:
+            externalIP = None
+            for address in resource.resourceAddresses:
+                print("address type: " + address[0])
+                if (address[0] == "external"):
+                    externalIP = address[1]
+            if externalIP is not None:
+                iperfResult = await self.runIperf(externalIP, drone)
+                iperfObjArr.append(iperfResult['iperfResults'])
         
-        iperfResult = await self.runIperf(externalIP, drone)
+
         #drone.radioMap['dataRate'] = iperfResult['iperfResults']['mbps']
         currentPosition = getCurrentPosition(drone)
         self.radioMap.add(currentPosition.lat, currentPosition.lon,self.currentHeading,iperfResult['iperfResults']['mbps'])
+        #drone.radioMap.lats = currentPosition['lat']
         print("RadioMapLength:")
         print(self.radioMap.length)
+        
+        #at the end append all the individual iperf results to the self array
+
         self.ActionStatus = "SUCCESS"
         return "waypoint_entry"
 
